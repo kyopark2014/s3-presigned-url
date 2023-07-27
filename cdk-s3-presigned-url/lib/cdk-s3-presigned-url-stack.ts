fc9a6d43f056e3ecc6333d2281779e3867181fbf
ksdyb.park <ksdyb@amazon.com>
2023-07-28 03:30:59 +0900

    update

---
+++ cdk-s3-presigned-url/lib/cdk-s3-presigned-url-stack.ts
@@ -0,0 +1,150 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
+import * as logs from "aws-cdk-lib/aws-logs"
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as opensearch from 'aws-cdk-lib/aws-opensearchservice';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+const debug = false;
+const stage = 'dev';
+const s3_prefix = 'docs';
+const projectName = "s3-presigned-url";
+const bucketName = `storage-for-${projectName}`;
+
+export class CdkS3PresignedUrlStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // s3 
+    const s3Bucket = new s3.Bucket(this, `storage-${projectName}`,{
+      bucketName: bucketName,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      versioned: false,
+    });
+    if(debug) {
+      new cdk.CfnOutput(this, 'bucketName', {
+        value: s3Bucket.bucketName,
+        description: 'The nmae of bucket',
+      });
+      new cdk.CfnOutput(this, 's3Arn', {
+        value: s3Bucket.bucketArn,
+        description: 'The arn of s3',
+      });
+      new cdk.CfnOutput(this, 's3Path', {
+        value: 's3://'+s3Bucket.bucketName,
+        description: 'The path of s3',
+      });
+    }
+
+    // copy web application files into s3 bucket
+    new s3Deploy.BucketDeployment(this, `upload-HTML-for-${projectName}`, {
+      sources: [s3Deploy.Source.asset("../html")],
+      destinationBucket: s3Bucket,
+    });
+
+    // cloudfront
+    const distribution = new cloudFront.Distribution(this, `cloudfront-for-${projectName}`, {
+      defaultBehavior: {
+        origin: new origins.S3Origin(s3Bucket),
+        allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+        viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      priceClass: cloudFront.PriceClass.PRICE_CLASS_200,  
+    });
+    new cdk.CfnOutput(this, `distributionDomainName-for-${projectName}`, {
+      value: distribution.domainName,
+      description: 'The domain name of the Distribution',
+    });
+
+
+    // Lambda - Upload
+    const lambdaUpload = new lambda.Function(this, `lambda-upload-for-${projectName}`, {
+      runtime: lambda.Runtime.NODEJS_16_X, 
+      functionName: `lambda-upload-for-${projectName}`,
+      code: lambda.Code.fromAsset("../lambda-upload"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(10),
+      logRetention: logs.RetentionDays.ONE_DAY,
+      environment: {
+        bucketName: s3Bucket.bucketName,
+        s3_prefix:  s3_prefix
+      }      
+    });
+    s3Bucket.grantReadWrite(lambdaUpload);
+
+    // role
+    const role = new iam.Role(this, `api-role-for-${projectName}`, {
+      roleName: `api-role-for-${projectName}`,
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com")
+    });
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: ['lambda:InvokeFunction']
+    }));
+    role.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/AWSLambdaExecute',
+    }); 
+
+    // API Gateway
+    const api = new apiGateway.RestApi(this, `api-chatbot-for-${projectName}`, {
+      description: 'API Gateway for chatbot',
+      endpointTypes: [apiGateway.EndpointType.REGIONAL],
+      binaryMediaTypes: ['application/pdf', 'text/plain', 'text/csv'], 
+      deployOptions: {
+        stageName: stage,
+
+        // logging for debug
+        loggingLevel: apiGateway.MethodLoggingLevel.INFO, 
+        dataTraceEnabled: true,
+      },
+    });  
+
+
+    
+    // POST method - upload
+    const resourceName = "upload";
+    const upload = api.root.addResource(resourceName);
+    upload.addMethod('POST', new apiGateway.LambdaIntegration(lambdaUpload, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      credentialsRole: role,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:true, 
+    }), {
+      methodResponses: [  
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+    if(debug) {
+      new cdk.CfnOutput(this, `ApiGatewayUrl-for-${projectName}`, {
+        value: api.url+'upload',
+        description: 'The url of API Gateway',
+      }); 
+    }
+
+    // cloudfront setting for api gateway    
+    distribution.addBehavior("/upload", new origins.RestApiOrigin(api), {
+      cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+      allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,  
+      viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    });    
+  }
+}
---
+++ cdk-s3-presigned-url/test/cdk-s3-presigned-url.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkS3PresignedUrl from '../lib/cdk-s3-presigned-url-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-s3-presigned-url-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkS3PresignedUrl.CdkS3PresignedUrlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
